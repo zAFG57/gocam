dc598adc0b1b6df28af6ade21531c6ba79fe972e
ludo <tt982914@gmail.com>
2025-04-22 23:28:04 +0200

    z

--- main.go
+++ main.go
@@ -2,35 +2,41 @@ package main
 
 import (
 	"fmt"
-	//"image"
-	//"image/color"
-	//"io/ioutil"
 	"log"
-	//"net/http"
 	"os"
-
 	"gocv.io/x/gocv"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("How to run:\n\tfacedetect-from-url [image URL] [image file]")
+	if len(os.Args) < 2 {
+		fmt.Println("Pour lancer le programe:\n\t./webCam [stream URL]")
 		return
 	}
 
-	// parse args
 	imageURL := os.Args[1]
-	saveFile := os.Args[2]
-
 
 	cam, err := gocv.VideoCaptureFile(imageURL)
+	window := gocv.NewWindow("camera")
+	defer window.Close()
 	if err != nil {
 		log.Fatalf("Error opening video stream or file: %v", err)
 	}
 	defer cam.Close()
 
 	img := gocv.NewMat()
-	for ;; {
+	lastFrames := [3]gocv.Mat{
+		gocv.NewMat(),
+		gocv.NewMat(),
+		gocv.NewMat(),
+	}
+	defer func() {
+		for _, frame := range lastFrames {
+			frame.Close()
+		}
+	}()
+	
+	
+	for iteration := uint8(0);; iteration = (iteration+1)%3{
 		if ok := cam.Read(&img); !ok {
 			fmt.Printf("Device closed: %v\n", err)
 			return
@@ -38,305 +44,79 @@ func main() {
 		if img.Empty() {
 			continue
 		}
-		break
+		clearFrames(img, &lastFrames, iteration)
+		window.IMShow(img)
+		window.WaitKey(1)
 	}
-
-	gocv.IMWrite(saveFile, img)
-	fmt.Printf("saved to %s\n", saveFile)
-}
-/*
-
-package main
-
-import (
-	"fmt"
-	"net/http"
-	"image"
-	"os"
-	"bufio"
-	"strings"
-	"bytes"
-	"image/jpeg"
-)
-
-func main() {
-	fmt.Println("début")
-
-	test("http://192.168.1.49:81/stream")
-
-
-/ *
-	img, err := fetchImage("http://192.168.1.49:81/stream")
-	if err != nil {
-		fmt.Println("ici ça plante :", err)
-		return
-	}
-	err = saveImageToFile(img, "image.jpg")
-	if err != nil {
-		fmt.Println("Erreur lors de la sauvegarde de l'image :", err)
-		return
-	}
-	fmt.Println("fin")
-	* /
 }
 
-func newFetchImage(url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return 
+func clearFrames(frame gocv.Mat, lastFrames *[3]gocv.Mat, iteration uint8) {
+	if lastFrames[0].Empty() || lastFrames[1].Empty() || lastFrames[2].Empty() {
+		shiftFrames(frame, lastFrames, iteration)
+		return
 	}
-	defer resp.Body.Close()
+	avgDiff := gocv.NewMat()
+	diff1 := gocv.NewMat()
+	diff2 := gocv.NewMat()
+	diff3 := gocv.NewMat()
+	defer avgDiff.Close()
+	defer diff1.Close()
+	defer diff2.Close()
+	defer diff3.Close()
+
+	gocv.AbsDiff((*lastFrames)[0], (*lastFrames)[1], &diff1)
+	gocv.AbsDiff((*lastFrames)[1], (*lastFrames)[2], &diff2)
+	gocv.AbsDiff((*lastFrames)[2], (*lastFrames)[0], &diff3)
+
+	// Add the differences together
+	gocv.Add(diff1, diff2, &avgDiff)
+	gocv.Add(avgDiff, diff3, &avgDiff)
+
+	// Divide by 3 to get the average
+	scalarMat := gocv.NewMatWithSizeFromScalar(gocv.NewScalar(3, 3, 3, 0), avgDiff.Rows(), avgDiff.Cols(), avgDiff.Type())
+	defer scalarMat.Close()
+	gocv.Divide(avgDiff, scalarMat, &avgDiff)
+
+	sumDiff := avgDiff.Sum()
+	rsum := sumDiff.Val1 + sumDiff.Val2 + sumDiff.Val3
+	if rsum < 255000*3 {
+		removeImpurty(frame,avgDiff, lastFrames)
+		shiftFrames(frame, lastFrames, iteration)
+		fmt.Printf("c'est ok: %v\n", rsum)
+	} else {
+		frame.CopyTo(&(*lastFrames)[0])
+		frame.CopyTo(&(*lastFrames)[1])
+		frame.CopyTo(&(*lastFrames)[2])
+		fmt.Printf("ça bouge trop: %v\n", rsum)
+	}
+}
+
+func removeImpurty(frame gocv.Mat, avgDiff gocv.Mat, lastFrames *[3]gocv.Mat) {
+	// Create a mask from the average difference
+	mask := gocv.NewMat()
+	defer mask.Close()
+	gocv.Threshold(avgDiff, &mask, 7, 255, gocv.ThresholdBinary)
+
+	// Set all pixels in the frame to 255 where the mask is non-zero
+	whiteMat := gocv.NewMatWithSize(frame.Rows(), frame.Cols(), frame.Type())
+	defer whiteMat.Close()
+	whiteMat.SetTo(gocv.NewScalar(255, 255, 255, 0))
 	
-	reader := bufio.NewReader(resp.Body)
-
-	var img string = ""
-	for {
-		line, _ := reader.ReadString('\n')
-		//fmt.Println(line)
-		if strings.Contains(line, "Content-Type: image/jpeg") {
-			//img = line
-			line, _ = reader.ReadString('\n')
-			fmt.Println("line", line)
-			line, _ = reader.ReadString('\n')
-			fmt.Println("line", line)
-			for {
-				line, _ = reader.ReadString('\n')
-				fmt.Println("line", line)
-				if strings.Contains(line, "--123456789000000000000987654321") {
-					fmt.Println(len(img))
-					break
+	// replace corrupted pixels with white
+	for y := 0; y < frame.Rows(); y++ {
+		for x := 0; x < frame.Cols(); x++ {
+			if mask.GetUCharAt(y, x) != 0 {
+				for i := 0; i < 3; i++ {
+					c1 := (*lastFrames)[0].GetUCharAt(y, x*frame.Channels()+i)
+					c2 := (*lastFrames)[1].GetUCharAt(y, x*frame.Channels()+i)
+					c3 := (*lastFrames)[2].GetUCharAt(y, x*frame.Channels()+i)
+					frame.SetUCharAt(y, x*frame.Channels()+i, uint8((int(c1)+int(c2)+int(c3))/3))
 				}
-				img +=  line
 			}
-			break
-		}	
-	}
-	fmt.Println("img", img)
-	
-	/ * Write the JPEG data directly to a file
-	file, err := os.Create("frame.jpg")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(img)
-	if err != nil {
-		fmt.Println("Error writing to file:", err)
-		return
-	}
-
-	fmt.Println("Frame saved as frame.jpg")
-	* /
-	
-	// Decode the JPEG image
-	imgDecode, err := jpeg.Decode(strings.NewReader(img))
-	if err != nil {
-		fmt.Println("Error decoding image:", err)
-		return
-	}
-
-	// Save the image
-	file, err := os.Create("frame.jpg")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	err = jpeg.Encode(file, imgDecode, nil)
-	if err != nil {
-		fmt.Println("Error saving image:", err)
-		return
-	}
-
-	fmt.Println("Frame saved as frame.jpg")
-}
-
-func fetchImage(url string) (image.Image, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("ha")
-		return nil, err
-	}
-	defer resp.Body.Close()
-	img, _, err := image.Decode(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode image: %v", err)
-	}
-	return img, nil
-}
-
-
-func fetchStreamImage(url string) (image.Image, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	
-	reader := bufio.NewReader(resp.Body)
-
-	for {
-		line, err := reader.ReadString('\n')
-		fmt.Println(line)
-		if strings.Contains(line, "Content-Type: image/jpeg") {
-			img := make([]byte, 0)
-			for {
-				line, err := reader.ReadBytes('\n')
-
-
-				fmt.Println("line", line)
-
-
-				if err != nil {
-					return nil, err
-				}
-				if strings.Contains(string(line), "--123456789000000000000987654321") && len(img) > 0 {
-					fmt.Println(img)
-					fmt.Println(len(img))
-					break
-				}
-				if strings.Contains(string(line), "--123456789000000000000987654321") {
-					fmt.Println("continue")
-					continue
-				}
-				img = append(img, line...)
-			}
-
-			imgReader := bytes.NewReader(img)
-			decodedImg, err := jpeg.Decode(imgReader)
-			if err != nil {
-				return nil, err
-			}
-			return decodedImg, nil
-		}
-		if err != nil {
-			return nil, err
 		}
 	}
 }
 
-func saveImageToFile(img image.Image, filename string) error {
-	out, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	err = jpeg.Encode(out, img, nil)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func shiftFrames(frame gocv.Mat, lastFrames *[3]gocv.Mat, iteration uint8) {
+	frame.CopyTo(&(*lastFrames)[iteration])
 }
-
-
-
-
-
-
-
-/*
-
-package main
-
-import (
-	"bufio"
-	"bytes"
-	"fmt"
-	//"image/jpeg"
-	"net/http"
-	"os"
-	"regexp"
-)
-
-func main() {
-	streamURL := "http://192.168.1.49:81/stream" // Replace with your ESP32-CAM IP
-
-	// Connect to the MJPEG stream
-	resp, err := http.Get(streamURL)
-	if err != nil {
-		fmt.Println("Error connecting to stream:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Create a buffered reader
-	reader := bufio.NewReader(resp.Body)
-
-	// Define MJPEG boundary regex
-	boundaryRegex := regexp.MustCompile(`--(\S+)`)
-
-	var imgBuffer bytes.Buffer
-	isCapturing := false
-
-	// Read the MJPEG stream
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err != nil {
-			fmt.Println("Error reading stream:", err)
-			break
-		}
-
-		// Check for the MJPEG boundary
-		if boundaryRegex.Match(line) {
-			if imgBuffer.Len() > 0 {
-				break // We have captured one frame
-			}
-			isCapturing = true
-			imgBuffer.Reset()
-			continue
-		}
-
-		// Capture JPEG data
-		if isCapturing {
-			imgBuffer.Write(line)
-		}
-	}
-
-	fmt.Println( imgBuffer.String())
-	fmt.Println( imgBuffer.Len())
-
-	// Write the JPEG data directly to a file
-	file, err := os.Create("frame.jpg")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	_, err = file.Write(imgBuffer.Bytes())
-	if err != nil {
-		fmt.Println("Error writing to file:", err)
-		return
-	}
-
-	fmt.Println("Frame saved as frame.jpg")
-	/*
-	// Decode the JPEG image
-	img, err := jpeg.Decode(bytes.NewReader(imgBuffer.Bytes()))
-	if err != nil {
-		fmt.Println("Error decoding image:", err)
-		return
-	}
-
-	// Save the image
-	file, err := os.Create("frame.jpg")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	err = jpeg.Encode(file, img, nil)
-	if err != nil {
-		fmt.Println("Error saving image:", err)
-		return
-	}
-
-	fmt.Println("Frame saved as frame.jpg")
-	* /
-}*/
---
+++ maskedImage.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gocv.io/x/gocv"
+)
+
+type maskedImage struct {
+	image *gocv.Mat
+}
